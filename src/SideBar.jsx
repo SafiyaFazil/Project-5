1068561e2386d9c962feaf63124598d7ea2d022e
Safiya@0619 <saficute05@gmail.com>
2023-09-13 10:31:44 +0530

    Project-5

--- src/SideBar.jsx
+++ src/SideBar.jsx
@@ -27,31 +27,31 @@ const SideBar = () => {
             <Col className="heading">Medico Sales</Col>
           </Row>
         </Container>
-        <Container className='nav'>
+        <Container className="nav">
           <Nav defaultActiveKey="/home" className="flex-column">
-            <Nav.Link eventKey="link-1">
+            <Nav.Link eventKey="link-1" className="navlink">
               <BiSolidGridAlt /> Dashboard
             </Nav.Link>
-            <Nav.Link eventKey="link-2">
+            <Nav.Link eventKey="link-2" className="navlink">
               <BiSolidCalendarCheck /> Lab Test
             </Nav.Link>
-            <Nav.Link eventKey="link-2">
+            <Nav.Link eventKey="link-2" className="navlink">
               <TbCalendarTime /> Appoinment
             </Nav.Link>
-            <Nav.Link eventKey="link-2">
+            <Nav.Link eventKey="link-2" className="navlink">
               <BsFillHandbagFill /> Medicine Order
             </Nav.Link>
-            <Nav.Link eventKey="link-2">
+            <Nav.Link eventKey="link-2" className="navlink">
               <BiMessage /> Message
             </Nav.Link>
-            <Nav.Link eventKey="link-2">
+            <Nav.Link eventKey="link-2" className="navlink">
               <LuWallet /> Payment
             </Nav.Link>
-            <Nav.Link eventKey="link-2">
+            <Nav.Link eventKey="link-2" className="navlink">
               <FiSettings /> Settings
             </Nav.Link>
             <div class="mb-auto p-2">
-              <Nav.Link eventKey="link-2">
+              <Nav.Link eventKey="link-2" className="navlink">
                 <BiHelpCircle /> Help
               </Nav.Link>
             </div>
